03a8be899674b17052c5c4993b33377343c55373
Jacob Bare <jacob@limit0.io>
2019-03-08 14:40:41 -0600

    Add initial EE site

---
+++ sites/evaluation-engineering/config/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+  logo: 'https://cdn.evaluationengineering.com/files/base/ebm/ee/image/static/logo/site_logo_bare.png',
+  primaryNavItems: [
+    { href: '/applications', label: 'Applications' },
+    { href: '/ee-blogs', label: 'EE Blogs' },
+    { href: '/industries', label: 'Industries' },
+    { href: '/industry-events', label: 'Industry Events' },
+    { href: '/new-products', label: 'New Products' },
+  ],
+};
