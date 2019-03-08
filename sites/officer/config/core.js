5c7cdc518eb4c6f25c1bb1a9ca301ab95c3ed61b
Jacob Bare <jacob@limit0.io>
2019-03-08 14:36:36 -0600

    Init officer site

---
+++ sites/officer/config/core.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteName: 'Officer',
+  locale: 'en_US',
+};
---
+++ sites/officer/config/site.js
@@ -0,0 +1,6 @@
+module.exports = {
+  logo: 'https://cdn.officer.com/files/base/cygnus/ofcr/image/static/logo/site_logo.png',
+  primaryNavItems: [
+    { href: '/command-hq', label: 'Command/HQ' },
+  ],
+};
