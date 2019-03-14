6c792a2902980c4b662a4cb54fbcdd1ae44cd89e
Jacob Bare <jacob@limit0.io>
2019-03-14 11:26:27 -0500

    Recreate officer

---
+++ sites/officer/config/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+  logo: 'https://cdn.officer.com/files/base/cygnus/ofcr/image/static/logo/site_logo.png',
+  primaryNavItems: [
+    { href: '/blogs', label: 'Blogs' },
+    { href: '/command-hq', label: 'Command/HQ' },
+    { href: '/features', label: 'Features' },
+  ],
+};
