762793615f0e56d13d9e3da258ba4a4f4ad23de1
Jacob Bare <jacob@limit0.io>
2019-03-14 11:23:33 -0500

    Recreate evaluation-engineering

---
+++ sites/evaluation-engineering/config/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+  logo: 'https://cdn.evaluationengineering.com/files/base/ebm/ee/image/static/logo/site_logo_bare.png',
+  primaryNavItems: [
+    { href: '/applications', label: 'Applications' },
+    { href: '/ee-blogs', label: 'EE Blogs' },
+    { href: '/industries', label: 'Industries' },
+    { href: '/industry-events', label: 'Industry Events' },
+    { href: '/instrumentation', label: 'Instrumentation' },
+    { href: '/new-products', label: 'New Products' },
+  ],
+};
