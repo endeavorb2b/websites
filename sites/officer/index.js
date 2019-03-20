af053561531d8a4cdd62396a0d77884f9ca796a7
Josh Worden <solocommand@gmail.com>
2019-03-20 13:35:12 -0500

    Do not include service URL in config

--- sites/officer/index.js
+++ sites/officer/index.js
@@ -2,11 +2,11 @@ const { startServer } = require('@base-cms/marko-web');
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
+const { log } = console;
 
 startServer({
   rootDir: __dirname,
   coreConfig,
   siteConfig,
   routes,
-  graphqlUri: 'https://ofcr.ebm.base-cms.io/graphql',
-});
+}).then(() => log('Started!')).catch(e => setImmediate(() => { throw e; }));
