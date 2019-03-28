5f26c2de2cb930ea59f2100ba5b186492041ddb7
Jacob Bare <jacob@limit0.io>
2019-03-28 13:07:26 -0500

    Add secondary nav items and ad unit config

--- sites/dentaleconomics/config/site.js
+++ sites/dentaleconomics/config/site.js
@@ -1,17 +1,34 @@
 module.exports = {
   logo: 'http://base.imgix.net/files/base/pennwell/de/logo.png',
   primaryNavItems: [
-    { href: '/dental-office-design', label: 'Dental Office Design' },
     { href: '/macro-op-ed', label: 'Macro/Op-Ed' },
-    { href: '/money', label: 'Money' },
     { href: '/practice', label: 'Practice' },
     { href: '/science-tech', label: 'Science & Tech' },
+    { href: '/money', label: 'Money' },
+    { href: '/dental-office-design', label: 'Dental Office Design' },
   ],
   secondaryNavItems: [
-    { href: '/dental-office-design', label: 'Dental Office Design' },
-    { href: '/macro-op-ed', label: 'Macro/Op-Ed' },
-    { href: '/money', label: 'Money' },
-    { href: '/practice', label: 'Practice' },
-    { href: '/science-tech', label: 'Science & Tech' },
+    { href: '/subscribe', label: 'Subscribe'},
+    { href: 'https://www.dentalacademyofce.com', label: 'Earn CE' },
+    { href: '/white-papers', label: 'White Papers' },
+    { href: '/videos', label: 'Videos' },
+    { href: '/job-listings', label: 'Job Listings' },
+    { href: 'https://www.dentistryiq.com/products/free-samples', label: 'Free Samples' },
   ],
+  ads: {
+    units: {
+      BS: {
+        path: '/21687441225/DE/DE_BS',
+        size: [[970, 250], [970, 90], [728, 90], [320, 50], [300, 50]],
+      },
+      MR: {
+        path: '/21687441225/DE/DE_MR',
+        size: [300, 250],
+      },
+      HP: {
+        path: '/21687441225/DE/DE_HP',
+        size: [300, 600],
+      },
+    },
+  },
 };
