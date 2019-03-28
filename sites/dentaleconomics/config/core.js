b58183aa17724bc6c3bda6d9165f248df4bbb5f7
Jacob Bare <jacob@limit0.io>
2019-03-28 13:06:11 -0500

    Create dental economics

---
+++ sites/dentaleconomics/config/core.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteName: 'Dental Economics',
+  locale: 'en_US',
+  images: {
+    lazyload: true,
+  },
+};
---
+++ sites/dentaleconomics/config/site.js
@@ -0,0 +1,17 @@
+module.exports = {
+  logo: 'http://base.imgix.net/files/base/pennwell/de/logo.png',
+  primaryNavItems: [
+    { href: '/dental-office-design', label: 'Dental Office Design' },
+    { href: '/macro-op-ed', label: 'Macro/Op-Ed' },
+    { href: '/money', label: 'Money' },
+    { href: '/practice', label: 'Practice' },
+    { href: '/science-tech', label: 'Science & Tech' },
+  ],
+  secondaryNavItems: [
+    { href: '/dental-office-design', label: 'Dental Office Design' },
+    { href: '/macro-op-ed', label: 'Macro/Op-Ed' },
+    { href: '/money', label: 'Money' },
+    { href: '/practice', label: 'Practice' },
+    { href: '/science-tech', label: 'Science & Tech' },
+  ],
+};
