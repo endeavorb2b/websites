41fc4f7fcbe6bc244f1c0d280a8768d9a7d77380
Jacob Bare <jacob@limit0.io>
2019-03-28 12:11:22 -0500

    Add secondary nav items and ad unit config

--- sites/utilityproducts/config/site.js
+++ sites/utilityproducts/config/site.js
@@ -1,18 +1,33 @@
 module.exports = {
+  logo: '',
   primaryNavItems: [
-    { href: '/line-construction-maintenance', label: 'Line Construction & Maintenance' },
-    { href: '/safety', label: 'Safety' },
-    { href: '/test-measurement', label: 'Test & Measurement' },
-    { href: '/tools-supplies', label: 'Tools & Supplies' },
     { href: '/transmission-distribution', label: 'Transmission & Distribution' },
     { href: '/vehicles-accessories', label: 'Vehicles & Accessories' },
+    { href: '/tools-supplies', label: 'Tools & Supplies' },
+    { href: '/safety', label: 'Safety' },
+    { href: '/line-construction-maintenance', label: 'Line Construction & Maintenance' },
+    { href: '/test-measurement', label: 'Test & Measurement' },
   ],
   secondaryNavItems: [
-    { href: '/line-construction-maintenance', label: 'Line Construction & Maintenance' },
-    { href: '/safety', label: 'Safety' },
-    { href: '/test-measurement', label: 'Test & Measurement' },
-    { href: '/tools-supplies', label: 'Tools & Supplies' },
-    { href: '/transmission-distribution', label: 'Transmission & Distribution' },
-    { href: '/vehicles-accessories', label: 'Vehicles & Accessories' },
+    { href: '/subscribe', label: 'Subscribe' },
+    { href: '/videos', label: 'Videos' },
+    { href: '/white-papers', label: 'White Papers' },
+    { href: '/events', label: 'Events' },
   ],
+  ads: {
+    units: {
+      BS: {
+        path: '/21687441225/UP/UP_BS',
+        size: [[970, 250], [970, 90], [728, 90], [300, 50]],
+      },
+      MR: {
+        path: '/21687441225/UP/UP_MR',
+        size: [300, 250],
+      },
+      HP: {
+        path: '/21687441225/UP/UP_HP',
+        size: [300, 600],
+      },
+    },
+  },
 };
