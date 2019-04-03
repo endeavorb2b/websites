71cfb4294ab714bd5176340cbce098e2fa377170
Jacob Bare <jacob@limit0.io>
2019-04-03 15:48:25 -0500

    Create initial common package

---
+++ packages/common/browser/navbar-toggle-button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    v-on:click="toggle"
+    v-bind:id="id"
+    v-bind:class="{ expanded: isExpanded }"
+    class="navbar-toggler"
+    type="button"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+      default: "primary-navbar-toggle"
+    },
+    targets: {
+      type: Array,
+    },
+    toggleClass: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isExpanded: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isExpanded = !this.isExpanded;
+      const elementList = document.querySelectorAll(this.targets.join(','));
+      elementList.forEach((element) => {
+        element.classList.toggle(this.toggleClass);
+      });
+    },
+  },
+};
+</script>
