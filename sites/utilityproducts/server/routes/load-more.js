d74570667b85cb3236401660c27f03d2fbc3f937
Jacob Bare <jacob@limit0.io>
2019-04-05 08:36:27 -0500

    Replace load more with common

--- sites/utilityproducts/server/routes/load-more.js
+++ sites/utilityproducts/server/routes/load-more.js
@@ -1,9 +1,9 @@
 const { withLoadMore } = require('@base-cms/marko-web/middleware');
-const contentSubPageA = require('../components/content-sub-page-a');
+const contentLoadMoreA = require('@endeavorb2b/base-website-common/components/content/blocks/query-load-more-a');
 
 // Register blocks that support load more...
 const blocks = {
-  'content-sub-page-a': contentSubPageA,
+  'content-query-load-more-a': contentLoadMoreA,
 };
 
 module.exports = (app) => {
