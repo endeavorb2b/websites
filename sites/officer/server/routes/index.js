d355383be4c55cd48d29bace44b906bcb4f59319
Jacob Bare <jacob@limit0.io>
2019-04-05 14:38:07 -0500

    Update officer to use new theme

--- sites/officer/server/routes/index.js
+++ sites/officer/server/routes/index.js
@@ -1,14 +1,12 @@
 const contentTypes = require('./content');
 const dynamicPages = require('./dynamic-page');
-const index = require('../templates/index');
+const home = require('./home');
 const loadMore = require('./load-more');
 const websiteSections = require('./website-section');
 
 module.exports = (app) => {
   // Homepage
-  app.get('/', (req, res) => {
-    res.marko(index);
-  });
+  home(app);
 
   // Load More / Infinite Scroll
   loadMore(app);
