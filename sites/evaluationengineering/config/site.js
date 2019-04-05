cb712c5c51710dff597e38877b7934c9abceab8e
Jacob Bare <jacob@limit0.io>
2019-04-05 14:52:24 -0500

    Update EE to new theme

--- sites/evaluationengineering/config/site.js
+++ sites/evaluationengineering/config/site.js
@@ -1,5 +1,10 @@
 module.exports = {
-  logo: 'https://cdn.evaluationengineering.com/files/base/ebm/ee/image/static/logo/site_logo_bare.png',
+  logo: {
+    src: 'https://cdn.evaluationengineering.com/files/base/ebm/ee/image/static/logo/site_logo_bare.png',
+    srcset: [
+      'https://cdn.evaluationengineering.com/files/base/ebm/ee/image/static/logo/site_logo_bare.png 2x',
+    ],
+  },
   primaryNavItems: [
     { href: '/applications', label: 'Applications' },
     { href: '/industries', label: 'Industries' },
@@ -7,9 +12,16 @@ module.exports = {
     { href: '/new-products', label: 'New Products' },
   ],
   secondaryNavItems: [
-    { href: '/applications', label: 'Applications' },
-    { href: '/industries', label: 'Industries' },
-    { href: '/instrumentation', label: 'Instrumentation' },
-    { href: '/new-products', label: 'New Products' },
+    { href: '/magazine', label: 'In Print' },
+    { href: '/subscribe', label: 'Subscribe' },
+    { href: '/advertise', label: 'Advertise' },
+    { href: '/page/about-us', label: 'About Us' },
   ],
+  ad: {
+    units: {
+      BS: { path: '/21687441225/EE/ee970x250', size: [[970, 250], [970, 90]] },
+      MR: { path: '/21687441225/EE/ee-mr', size: [300, 250] },
+      HP: { path: '/21687441225/EE/ee-hp', size: [300, 600] },
+    },
+  },
 };
