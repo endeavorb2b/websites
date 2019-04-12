37ad40f7c3fc5edae27da771fa3468068ffe9309
Josh Worden <solocommand@gmail.com>
2019-04-12 11:20:30 -0500

    Add sitemaps to robots.txt

---
+++ sites/robots.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const { readdirSync, statSync } = require('fs')
+const { writeFileSync, existsSync } = require('fs');
+const { join } = require('path');
+
+const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
+const siteDirectories = dirs(__dirname);
+const sites = require('../sites');
+
+siteDirectories.forEach((k) => {
+  const { domain } = sites[k];
+  const robots = `# www.robotstxt.org/
+# www.google.com/support/webmasters/bin/answer.py?hl=en&answer=156449
+
+User-agent: *
+Disallow: /
+
+Sitemap: https://www.${domain}/sitemap.xml
+Sitemap: https://www.${domain}/sitemap-google-news.xml`;
+
+  writeFileSync(join(__dirname, k, 'server/public/robots.txt'), robots);
+});
