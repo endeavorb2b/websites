4ffc2c6f51d5600d07dc1a8f59760e60402d6140
Jacob Bare <jacob@limit0.io>
2019-04-12 10:30:00 -0500

    Add WebsiteSectionPageFragment to section route

--- sites/strategies-u/server/routes/website-section.js
+++ sites/strategies-u/server/routes/website-section.js
@@ -1,8 +1,19 @@
+const gql = require('graphql-tag');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
+
+const queryFragment = gql`
+  fragment WebsiteSectionPageFragment on WebsiteSection {
+    hierarchy {
+      id
+      alias
+    }
+  }
+`;
 
 module.exports = (app) => {
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
+    queryFragment,
   }));
 };
