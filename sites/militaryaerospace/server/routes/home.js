beb3894be24a5db17e6815e1c7f792262f9120c7
Jacob Bare <jacob@limit0.io>
2019-04-12 10:28:06 -0500

    Add HomePageFragment to home route

--- sites/militaryaerospace/server/routes/home.js
+++ sites/militaryaerospace/server/routes/home.js
@@ -1,9 +1,20 @@
+const gql = require('graphql-tag');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const home = require('../templates/index');
+
+const queryFragment = gql`
+  fragment HomePageFragment on WebsiteSection {
+    hierarchy {
+      id
+      alias
+    }
+  }
+`;
 
 module.exports = (app) => {
   app.get('/', withWebsiteSection({
     aliasResolver: () => 'home',
     template: home,
+    queryFragment,
   }));
 };
