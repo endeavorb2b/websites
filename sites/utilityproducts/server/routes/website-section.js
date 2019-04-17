20533329dd0a1bd4946c92d3f88d16840065a842
Jacob Bare <jacob@limit0.io>
2019-04-17 09:50:18 -0500

    Home page layout experiments on UP

--- sites/utilityproducts/server/routes/website-section.js
+++ sites/utilityproducts/server/routes/website-section.js
@@ -1,8 +1,20 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
+const index = require('../templates/index');
+const index3 = require('../templates/index3');
 
 module.exports = (app) => {
+  app.get('/:alias(transmission-distribution)', withWebsiteSection({
+    template: index,
+    queryFragment,
+  }));
+
+  app.get('/:alias(vehicles-accessories)', withWebsiteSection({
+    template: index3,
+    queryFragment,
+  }));
+
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
