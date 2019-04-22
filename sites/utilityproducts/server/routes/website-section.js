8dda57795b47ea2271260915684be778bb09b695
Brandon Krigbaum <brandonbk@gmail.com>
2019-04-22 11:11:37 -0500

    UP ad unit updates

--- sites/utilityproducts/server/routes/website-section.js
+++ sites/utilityproducts/server/routes/website-section.js
@@ -1,14 +1,8 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
-const indexAlt = require('../templates/index-alt');
 
 module.exports = (app) => {
-  app.get('/:alias(transmission-distribution)', withWebsiteSection({
-    template: indexAlt,
-    queryFragment,
-  }));
-
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
