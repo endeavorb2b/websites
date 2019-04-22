e52d6cdb502a7c7c9d0c26b511dc7ccbefc7fe56
Josh Worden <solocommand@gmail.com>
2019-04-22 12:29:03 -0500

    Support company primary imge

--- packages/themes/pennwell/api/fragments/all-published-content.js
+++ packages/themes/pennwell/api/fragments/all-published-content.js
@@ -20,12 +20,41 @@ fragment AllPublishedContentQueryFragment on Content {
     id
     src
   }
+  ... on ContentWhitepaper {
+    company {
+      id
+      name
+      canonicalPath
+      primaryImage {
+        id
+        src
+      }
+    }
+  }
   ... on ContentWebinar {
     starts
+    company {
+      id
+      name
+      canonicalPath
+      primaryImage {
+        id
+        src
+      }
+    }
   }
   ... on ContentEvent {
     starts
     ends
+    company {
+      id
+      name
+      canonicalPath
+      primaryImage {
+        id
+        src
+      }
+    }
   }
 }
 
