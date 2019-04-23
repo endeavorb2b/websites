2b3ce3b618b3bee8a638c840a64cf2bf00bef993
Josh Worden <solocommand@gmail.com>
2019-04-23 15:53:39 -0500

    routes/templates for ebm sites

--- sites/plasticsmachinerymagazine/server/routes/subscribe.js
+++ sites/plasticsmachinerymagazine/server/routes/subscribe.js
@@ -1,12 +1,20 @@
+const { withMagazineSubscribeUrl } = require('@base-cms/marko-web/middleware');
+const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/magazine-publication');
+const subscribe = require('../templates/subscribe/index');
+const subscribeEmail = require('../templates/subscribe/email');
 const subscribeMagazine = require('../templates/subscribe/magazine');
 
 module.exports = (app) => {
-  // app.get('/subscribe', (req, res) => {
-  //   res.marko(subscribe);
-  // });
-
-  app.get('/subscribe/print', (req, res) => {
-    const { key } = req.params;
-    res.marko(subscribeMagazine, { key });
+  app.get('/subscribe', (_, res) => {
+    res.marko(subscribe);
   });
+
+  app.get('/subscribe/email', (_, res) => {
+    res.marko(subscribeEmail);
+  });
+
+  app.get(':subscribeUrl(/subscribe/print/[a-zA-Z]+)', withMagazineSubscribeUrl({
+    template: subscribeMagazine,
+    queryFragment,
+  }));
 };
