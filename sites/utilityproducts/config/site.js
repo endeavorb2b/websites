5fefecd94852605608dd2f00d91972acb4ff0ffa
Josh Worden <solocommand@gmail.com>
2019-04-23 15:44:45 -0500

    update urls

--- sites/utilityproducts/config/site.js
+++ sites/utilityproducts/config/site.js
@@ -56,6 +56,12 @@ module.exports = {
     engine: 'google',
     apiKey: '003355913687346718228:mb2grfhfx08',
   },
+  subscriptions: {
+    newsletters: 'https://formdesigner.ecn5.com/GetForm?tokenuid=6203a757-d517-4d5e-91d5-85e0f236559c',
+    publications: {
+      '/subscribe/print/up': 'http://formdesigner.ecn5.com/GetForm?tokenuid=a9514072-dfbd-488d-8148-b0477928fed8&promoCode=NW8301&cmpid=website_sub_up_nw8301_mar-14-2018',
+    },
+  },
   magazines: {
     name: 'Utility Products',
     subscribeUrl: 'http://formdesigner.ecn5.com/GetForm?tokenuid=a9514072-dfbd-488d-8148-b0477928fed8&promoCode=NW8301&cmpid=website_sub_up_nw8301_mar-14-2018',
