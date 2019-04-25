70e380c793ca369605bb24a19350002947671baf
Josh Worden <solocommand@gmail.com>
2019-04-25 14:43:18 -0500

    Add NR to sites

--- sites/militaryaerospace/index.js
+++ sites/militaryaerospace/index.js
@@ -1,6 +1,7 @@
 const { startServer } = require('@base-cms/marko-web');
 const onStart = require('@endeavorb2b/base-website-common/on-start');
 const errorTemplate = require('@endeavorb2b/base-website-themes/pennwell/templates/error');
+require('./newrelic');
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
