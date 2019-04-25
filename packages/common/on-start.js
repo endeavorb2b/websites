e3b94eab7ea5239b97b7ba3da059329e39644adb
Josh Worden <solocommand@gmail.com>
2019-04-25 15:55:23 -0500

    Revert NR in common package

--- packages/common/on-start.js
+++ packages/common/on-start.js
@@ -1,14 +1,7 @@
 const helmet = require('helmet');
-const newrelic = require('newrelic');
 
 // Set global express settings/middlewares for _all_ websites that use this function.
 module.exports = (app) => {
   app.set('trust proxy', 'loopback, linklocal, uniquelocal');
   app.use(helmet());
-
-  // NR error logging
-  app.use((err, req, res, next) => {
-    newrelic.noticeError(err);
-    next(err);
-  });
 };
