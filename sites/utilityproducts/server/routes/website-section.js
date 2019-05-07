653b52748f4272fbb1610bcb91710a7100cfb557
Josh Worden <solocommand@gmail.com>
2019-05-07 11:52:12 -0500

    Remove website section route

--- sites/utilityproducts/server/routes/website-section.js
+++ sites/utilityproducts/server/routes/website-section.js
@@ -1,14 +1,8 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
-const contactUs = require('../templates/website-section/contact-us');
 
 module.exports = (app) => {
-  app.get('/:alias(contact-us)', withWebsiteSection({
-    template: contactUs,
-    queryFragment,
-  }));
-
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
