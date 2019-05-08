2d4e40c3e78a6c6eabf29a4ffe8245c2b768d7a5
Jacob Bare <jacob@limit0.io>
2019-05-08 10:26:09 -0400

    Add UP NativeX site config

---
+++ sites/utilityproducts/config/native-x.js
@@ -0,0 +1,12 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    listA: {
+      id: '5ccc5ca6999679000175baaf',
+    },
+    listB: {
+      id: '5ccc536739937800016e7094',
+    },
+  },
+};
