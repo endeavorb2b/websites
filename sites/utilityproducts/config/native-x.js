5c1428adb230c6be3e4359de9f58189f46822de7
Jacob Bare <jacob@limit0.io>
2019-05-10 12:59:38 -0500

    Update UP nativex config for topic placements

--- sites/utilityproducts/config/native-x.js
+++ sites/utilityproducts/config/native-x.js
@@ -2,14 +2,18 @@ module.exports = {
   enabled: true,
   uri: 'https://ebm.native-x.io',
   placements: {
-    listA: {
-      id: '5ccc5ca6999679000175baaf',
+    default: {
+      // fluid (21:9) top image
+      card: { id: '5cd44bb5f41dfb0001350efe' },
+      // 75x75 right image
+      list1: { id: '5ccc5ca6999679000175baaf' },
+      // 176x99 (16:9) left image
+      list2: { id: '5ccc536739937800016e7094' },
     },
-    listB: {
-      id: '5ccc536739937800016e7094',
-    },
-    card: {
-      id: '5cd44bb5f41dfb0001350efe',
+    'transmission-distribution': {
+      card: { id: '5cd5862ff41dfb00015a56cf' },
+      list1: { id: '5cd5863bf41dfb00015a5b10' },
+      list2: { id: '5cd5865f39937800016f0eef' },
     },
   },
 };
