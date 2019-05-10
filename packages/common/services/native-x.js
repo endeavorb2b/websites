7ad9624d3eb661dbe4d0dd02bdb929bb1436585e
Jacob Bare <jacob@limit0.io>
2019-05-10 12:48:53 -0500

    Use placement name and aliases for nativex

--- packages/common/services/native-x.js
+++ packages/common/services/native-x.js
@@ -1,4 +1,4 @@
-const { isObject } = require('@base-cms/utils');
+const { isObject, asArray } = require('@base-cms/utils');
 const fetch = require('node-fetch');
 
 const retrieve = async ({ uri, placementId, opts }) => {
@@ -15,22 +15,30 @@ const retrieve = async ({ uri, placementId, opts }) => {
 
 const getURI = site => site.get('nativeX.uri');
 
-const getPlacementId = (name, site) => site.get(`nativeX.placements.${name}.id`);
+const getPlacementId = (config, { name, aliases }) => {
+  const prefix = 'nativeX.placements';
+  const defaultId = config.get(`${prefix}.default.${name}.id`);
+  const foundId = asArray(aliases).map(alias => config.get(`${prefix}.${alias}.${name}.id`)).filter(v => v)[0];
+  return foundId || defaultId;
+};
 
 const getEnabled = site => site.get('nativeX.enabled', true);
 
 const isInitable = site => getURI(site) && getEnabled(site);
 
-const isEnabled = (placementName, site) => {
-  const enabled = getEnabled(site);
-  const uri = getURI(site);
-  const id = getPlacementId(placementName, site);
-  return uri && id && enabled;
+const isEnabled = (config, { name, aliases }) => {
+  if (!name) return false;
+  const enabled = getEnabled(config);
+  const uri = getURI(config);
+  const id = getPlacementId(config, { name, aliases });
+  if (uri && id && enabled) return true;
+  return false;
 };
 
 const hasIndex = index => typeof index !== 'undefined' && index !== null && index >= 0;
 
-const useNativeX = (name, index, site) => isEnabled(name, site) && hasIndex(index);
+const useNativeX = (config, { name, index, aliases }) => isEnabled(config, { name, aliases })
+  && hasIndex(index);
 
 module.exports = {
   isEnabled,
