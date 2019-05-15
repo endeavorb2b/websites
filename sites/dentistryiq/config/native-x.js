1b6ce72f341a8f2cd556d5fea1a58e8e5b5492b4
Jacob Bare <jacob@limit0.io>
2019-05-15 08:32:35 -0500

    Create NativeX config for dentistryiq

---
+++ sites/dentistryiq/config/native-x.js
@@ -0,0 +1,41 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    default: {
+      card: { id: '5cdb19ca39937800010d19fc' },
+      list2: { id: '5cdb19cf39937800010d1ae8' },
+      list1: { id: '5cdb19d5f41dfb0001fb5519' },
+    },
+    clinical: {
+      card: { id: '5cdb1a1af41dfb0001fb636a' },
+      list2: { id: '5cdb1a22f41dfb0001fb6511' },
+      list1: { id: '5cdb1a2939937800010d2e09' },
+    },
+    'dental-assisting': {
+      card: { id: '5cdb1a9139937800010d416f' },
+      list2: { id: '5cdb1a9c39937800010d4622' },
+      list1: { id: '5cdb1aa639937800010d47cb' },
+    },
+    'dental-hygiene': {
+      card: { id: '5cdb19f1f41dfb0001fb5bb5' },
+      list2: { id: '5cdb19f8f41dfb0001fb5d4f' },
+      list1: { id: '5cdb19fff41dfb0001fb5e6a' },
+    },
+    'practice-management': {
+      card: { id: '5cdb1a5439937800010d35e3' },
+      list2: { id: '5cdb1a5b39937800010d37a5' },
+      list1: { id: '5cdb1a6439937800010d3971' },
+    },
+    products: {
+      card: { id: '5cdb1a33f41dfb0001fb69d6' },
+      list2: { id: '5cdb1a3a39937800010d3110' },
+      list1: { id: '5cdb1a4439937800010d32ae' },
+    },
+    'front-office': {
+      card: { id: '5cdb1a7339937800010d3c98' },
+      list2: { id: '5cdb1a7bf41dfb0001fb7844' },
+      list1: { id: '5cdb1a83f41dfb0001fb799b' },
+    },
+  },
+};
