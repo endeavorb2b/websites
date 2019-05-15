05def1921daeab572926d9b32e61f067b2adc04b
Jacob Bare <jacob@limit0.io>
2019-05-15 08:33:51 -0500

    Create NativeX config for waterworld

---
+++ sites/waterworld/config/native-x.js
@@ -0,0 +1,31 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    default: {
+      card: { id: '5cdb1903f41dfb0001fb2ea7' },
+      list2: { id: '5cdb1909f41dfb0001fb2fdf' },
+      list1: { id: '5cdb190ff41dfb0001fb312c' },
+    },
+    municipal: {
+      card: { id: '5cdb16baf41dfb0001fac272' },
+      list2: { id: '5cdb16cc39937800010c8e9e' },
+      list1: { id: '5cdb16d4f41dfb0001fac7d7' },
+    },
+    'municipal/technologies': {
+      card: { id: '5cdb171639937800010c9aff' },
+      list2: { id: '5cdb172239937800010c9d0a' },
+      list1: { id: '5cdb172939937800010c9ea2' },
+    },
+    industrial: {
+      card: { id: '5cdb16e039937800010c9211' },
+      list2: { id: '5cdb16e639937800010c931f' },
+      list1: { id: '5cdb16eef41dfb0001facbb9' },
+    },
+    international: {
+      card: { id: '5cdb16fe39937800010c96d0' },
+      list2: { id: '5cdb170539937800010c9814' },
+      list1: { id: '5cdb170c39937800010c9948' },
+    },
+  },
+};
