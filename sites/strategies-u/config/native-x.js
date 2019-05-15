bf603ecbd449c876c25ed016a2ff844518fd3977
Jacob Bare <jacob@limit0.io>
2019-05-15 15:12:05 -0500

    Create NativeX config for strategies-u

---
+++ sites/strategies-u/config/native-x.js
@@ -0,0 +1,31 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    default: {
+      list1: { id: '5cdc6f87399378000137dd30' },
+      list2: { id: '5cdc6f81f41dfb00012647ae' },
+      card: { id: '5cdc6f7bf41dfb00012645e4' },
+    },
+    reports: {
+      list1: { id: '5cdc6f9f399378000137e26a' },
+      list2: { id: '5cdc6f97f41dfb0001264ccd' },
+      card: { id: '5cdc6f91399378000137df3c' },
+    },
+    analysts: {
+      list1: { id: '5cdc6fb6399378000137e860' },
+      list2: { id: '5cdc6faff41dfb0001265265' },
+      card: { id: '5cdc6faa399378000137e4e4' },
+    },
+    blogs: {
+      list1: { id: '5cdc6fccf41dfb0001265a53' },
+      list2: { id: '5cdc6fc5399378000137ec32' },
+      card: { id: '5cdc6fbff41dfb000126561d' },
+    },
+    'press-releases': {
+      list1: { id: '5cdc6fe4399378000137f4ed' },
+      list2: { id: '5cdc6fdcf41dfb0001265e8e' },
+      card: { id: '5cdc6fd6399378000137f17d' },
+    },
+  },
+};
