7ca41454472bbcd8aac5fdc23da20a537f140b26
Jacob Bare <jacob@limit0.io>
2019-05-15 15:07:43 -0500

    Create NativeX config for offshore-mag

---
+++ sites/offshore-mag/config/native-x.js
@@ -0,0 +1,51 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    default: {
+      list1: { id: '5cdc6e85f41dfb0001260144' },
+      list2: { id: '5cdc6e7ef41dfb000125ff04' },
+      card: { id: '5cdc6e78f41dfb000125fc1a' },
+    },
+    'drilling-completion': {
+      list1: { id: '5cdc6e9f3993780001379e64' },
+      list2: { id: '5cdc6e98f41dfb000126074d' },
+      card: { id: '5cdc6e91f41dfb00012604ce' },
+    },
+    'field-development': {
+      list1: { id: '5cdc6eb6f41dfb0001260f23' },
+      list2: { id: '5cdc6eb0399378000137a28b' },
+      card: { id: '5cdc6ea9f41dfb0001260c2d' },
+    },
+    subsea: {
+      list1: { id: '5cdc6ecb399378000137a92c' },
+      list2: { id: '5cdc6ed4399378000137ab89' },
+      card: { id: '5cdc6ec0f41dfb000126118c' },
+    },
+    geosciences: {
+      list1: { id: '5cdc6eec399378000137b21c' },
+      list2: { id: '5cdc6ee5399378000137b03f' },
+      card: { id: '5cdc6edff41dfb0001261a5d' },
+    },
+    pipelines: {
+      list1: { id: '5cdc6f04f41dfb0001262431' },
+      list2: { id: '5cdc6efcf41dfb000126225a' },
+      card: { id: '5cdc6ef5399378000137b506' },
+    },
+    production: {
+      list1: { id: '5cdc6f1f399378000137bfc3' },
+      list2: { id: '5cdc6f18f41dfb000126294e' },
+      card: { id: '5cdc6f10399378000137bb3c' },
+    },
+    'rigs-vessels': {
+      list1: { id: '5cdc6f41399378000137c8a9' },
+      list2: { id: '5cdc6f3af41dfb00012631ff' },
+      card: { id: '5cdc6f2bf41dfb0001262e4b' },
+    },
+    deepwater: {
+      list1: { id: '5cdc6f5af41dfb0001263b3b' },
+      list2: { id: '5cdc6f54399378000137cd71' },
+      card: { id: '5cdc6f4d399378000137cba1' },
+    },
+  },
+};
