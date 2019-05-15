183d83330a539300301a17c3449e8006acc3c73c
Jacob Bare <jacob@limit0.io>
2019-05-15 08:33:28 -0500

    Create NativeX config for rdhmag

---
+++ sites/rdhmag/config/native-x.js
@@ -0,0 +1,36 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    default: {
+      card: { id: '5cdb1abb39937800010d4ba2' },
+      list2: { id: '5cdb1ac039937800010d4ca7' },
+      list1: { id: '5cdb1ac639937800010d4d9e' },
+    },
+    'infection-control': {
+      card: { id: '5cdb1af8f41dfb0001fb9291' },
+      list2: { id: '5cdb1afff41dfb0001fb93ed' },
+      list1: { id: '5cdb1b0c39937800010d5b41' },
+    },
+    ergonomics: {
+      card: { id: '5cdb1ada39937800010d51a2' },
+      list2: { id: '5cdb1ae539937800010d53f7' },
+      list1: { id: '5cdb1aeef41dfb0001fb904c' },
+    },
+    'patient-care': {
+      card: { id: '5cdb1b2e39937800010d625f' },
+      list2: { id: '5cdb1b3af41dfb0001fb9fc5' },
+      list1: { id: '5cdb1b5439937800010d6866' },
+    },
+    pathology: {
+      card: { id: '5cdb1b6e39937800010d6e01' },
+      list2: { id: '5cdb1b81f41dfb0001fbad38' },
+      list1: { id: '5cdb1b8a39937800010d7458' },
+    },
+    'career-profession': {
+      card: { id: '5cdb1baaf41dfb0001fbb3e4' },
+      list2: { id: '5cdb1bb139937800010d795a' },
+      list1: { id: '5cdb1bb7f41dfb0001fbb606' },
+    },
+  },
+};
