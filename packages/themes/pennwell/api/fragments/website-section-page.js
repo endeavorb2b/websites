02c7cce0992f50a20a6c2c338b3d93b6f8bc3b37
Josh Worden <solocommand@gmail.com>
2019-05-17 14:36:31 -0500

    Change default layout

--- packages/themes/pennwell/api/fragments/website-section-page.js
+++ packages/themes/pennwell/api/fragments/website-section-page.js
@@ -2,9 +2,12 @@ const gql = require('graphql-tag');
 
 module.exports = gql`
 fragment WebsiteSectionPageFragment on WebsiteSection {
+  name
+  description
   hierarchy {
     id
     alias
+    name
   }
 }
 `;
