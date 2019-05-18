f9b954a426e0c5bcf2d4a902505f72b1ecd5ef0b
Brandon Krigbaum <brandonbk@gmail.com>
2019-05-17 21:45:12 -0500

    Analysts support for SU leveraging contact-us

--- sites/strategies-u/server/routes/website-section.js
+++ sites/strategies-u/server/routes/website-section.js
@@ -1,8 +1,13 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
+const analystsTemplate = require('../templates/contact-us');
 
 module.exports = (app) => {
+  app.get('/:alias(analysts)', withWebsiteSection({
+    template: analystsTemplate,
+    queryFragment,
+  }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
