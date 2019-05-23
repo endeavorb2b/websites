49a4053301a6941c3d640198a277a39158cc77ce
Josh Worden <solocommand@gmail.com>
2019-05-23 11:49:52 -0500

    Update Content ID routing

--- sites/profoodworld/server/routes/content.js
+++ sites/profoodworld/server/routes/content.js
@@ -3,7 +3,7 @@ const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
 
 module.exports = (app) => {
-  app.get('/:prefix(*):id(\\d{8}):suffix(*)', withContent({
+  app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
   }));
