3cdf547fecb44afabd2bdcc85e27dce53a3b6f2a
Josh Worden <solocommand@gmail.com>
2019-05-24 14:44:08 -0500

    Notify when deployments start

---
+++ scripts/deploy-notify-start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+payload="{
+  \"attachments\": [{
+    \"color\": \"#1d9bd1\",
+    \"text\": \"Deployment of \`$TRAVIS_REPO_SLUG\` @ \`$TRAVIS_TAG\` to production has started.\"
+  }]
+}"
+curl -f -X POST --data "$payload" \
+  -H 'Content-type: application/json' \
+  https://hooks.slack.com/services/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
+
+payload="{
+  \"deployment\": {
+    \"revision\": \"\`$TRAVIS_TAG\`\",
+    \"user\": \"TravisCD\"
+  }
+}"
+curl -f -X POST --data "$payload" \
+  -H 'Content-type: application/json' \
+  -H "X-Api-Key:$NR_APIKEY" \
+  https://api.newrelic.com/v2/applications/222873263/deployments.json
