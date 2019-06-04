b763b3b509b4fab9f6bbde8cd11a4f884086be76
jlaird <Jeremy.Laird@cygnus.com>
2019-06-04 15:16:45 -0500

    Fixed structure and class

--- sites/offshore-mag/server/routes/website-section.js
+++ sites/offshore-mag/server/routes/website-section.js
@@ -1,7 +1,7 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
-const mapspostersTemplate = require('../templates/website-section/resources-maps-posters');
+const mapspostersTemplate = require('../templates/website-section/resources/maps-posters');
 
 module.exports = (app) => {
   app.get('/:alias(resources/maps-posters)', withWebsiteSection({
