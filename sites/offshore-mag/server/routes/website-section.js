a04552cde7fd0e2b5c8af8c8ecc3ea7dae0db088
jlaird <Jeremy.Laird@cygnus.com>
2019-06-04 15:23:46 -0500

    Updated const name

--- sites/offshore-mag/server/routes/website-section.js
+++ sites/offshore-mag/server/routes/website-section.js
@@ -1,11 +1,11 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
-const mapspostersTemplate = require('../templates/website-section/resources/maps-posters');
+const mapsPostersTemplate = require('../templates/website-section/resources/maps-posters');
 
 module.exports = (app) => {
   app.get('/:alias(resources/maps-posters)', withWebsiteSection({
-    template: mapspostersTemplate,
+    template: mapsPostersTemplate,
     queryFragment,
   }));
 
