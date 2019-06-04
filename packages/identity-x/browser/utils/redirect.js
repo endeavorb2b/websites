095e142831b72fabbb0e35b37b1da97b444d23a7
Jacob Bare <jacob@limit0.io>
2019-06-04 14:42:17 -0500

    Use clean path util when redirecting

--- packages/identity-x/browser/utils/redirect.js
+++ packages/identity-x/browser/utils/redirect.js
@@ -1,5 +1,7 @@
+import cleanPath from './clean-path';
+
 export default (window, to) => {
-  const path = to.replace(/^\/+/, '');
+  const path = cleanPath(to);
   const url = `${window.location.origin}/${path}`;
   window.location.replace(url);
 };
