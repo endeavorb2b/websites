a76dffa851c17220048760a642e969a56a91ef08
Jacob Bare <jacob@limit0.io>
2019-06-04 10:05:14 -0500

    Create authenticate vue component

---
+++ packages/identity-x/browser/components/authenticate.vue
@@ -0,0 +1,69 @@
+<template>
+  <div v-if="redirecting">
+    <p>You've successfully logged in. Redirecting you...</p>
+  </div>
+  <div v-else-if="loading">
+    <p>Logging in...</p>
+  </div>
+  <div v-else-if="error">
+    <p>An error occured!</p>
+    <p>{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    token: {
+      type: String,
+      required: true,
+    },
+    redirectTo: {
+      type: String,
+      required: true,
+      default: '/',
+    },
+  },
+  data: () => ({
+    error: null,
+    loading: false,
+    redirecting: false,
+  }),
+  mounted() {
+    this.authenticate();
+  },
+  methods: {
+    /**
+     *
+     */
+    async authenticate() {
+      this.loading = true;
+      try {
+        const { token } = this;
+        if (!token) throw new Error('No login token was provided.');
+
+        // @todo This needs to be configured.
+        const res = await fetch('/__idx/authenticate', {
+          method: 'post',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify({ token }),
+        });
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.message);
+        this.redirect();
+      } catch (e) {
+        this.error = `Unable to login: ${e.message}`;
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    redirect() {
+      this.redirecting = true;
+      const path = this.redirectTo.replace(/^\/+/, '');
+      const url = `${window.location.origin}/${path}`;
+      window.location.replace(url);
+    },
+  },
+};
+</script>
