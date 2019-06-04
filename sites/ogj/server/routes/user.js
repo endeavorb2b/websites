69f117bf1a221709eb8fe7f7dce4344c91943b70
Jacob Bare <jacob@limit0.io>
2019-06-04 11:51:45 -0500

    Add logout route/template for OGJ

--- sites/ogj/server/routes/user.js
+++ sites/ogj/server/routes/user.js
@@ -1,5 +1,6 @@
-const login = require('../templates/user/login');
 const authenticate = require('../templates/user/authenticate');
+const login = require('../templates/user/login');
+const logout = require('../templates/user/logout');
 
 module.exports = (app) => {
   app.get('/user/authenticate', (req, res) => {
@@ -8,5 +9,9 @@ module.exports = (app) => {
 
   app.get('/user/login', (req, res) => {
     res.marko(login);
+  });
+
+  app.get('/user/logout', (req, res) => {
+    res.marko(logout);
   });
 };
