f2503ba515e3c242c05fe1e4b0c7b3455a860006
jlaird <Jeremy.Laird@cygnus.com>
2019-06-04 14:46:08 -0500

    Added new page for maps and posters

--- sites/offshore-mag/server/routes/website-section.js
+++ sites/offshore-mag/server/routes/website-section.js
@@ -1,8 +1,14 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
+const mapspostersTemplate = require('../templates/website-section/resources-maps-posters');
 
 module.exports = (app) => {
+  app.get('/:alias(resources/maps-posters)', withWebsiteSection({
+    template: mapspostersTemplate,
+    queryFragment,
+  }));
+
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
