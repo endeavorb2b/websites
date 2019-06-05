b8b80920351e08f5fa11d61ae0150ddf26b3cba9
Jacob Bare <jacob@limit0.io>
2019-06-05 14:23:12 -0500

    Remove direct dep on cookie-parser
    marko-web already includes this globally

--- packages/identity-x/routers/actions/index.js
+++ packages/identity-x/routers/actions/index.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const { json } = require('body-parser');
-const cookieParser = require('cookie-parser');
 const authenticate = require('./authenticate');
 const login = require('./login');
 const logout = require('./logout');
@@ -9,7 +8,6 @@ const jsonErrorHandler = require('../json-error-handler');
 const router = Router();
 
 router.use(json());
-router.use(cookieParser());
 router.post('/authenticate', authenticate);
 router.post('/login', login);
 router.post('/logout', logout);
