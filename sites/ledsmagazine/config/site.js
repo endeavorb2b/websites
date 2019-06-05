a8c2b2a696f3c74095873b6104042bbf01354756
Alysha Buselt <AlyshaB@Pennwell.com>
2019-06-05 09:17:31 -0500

    Updated the subscribe form for LEDs magazine

--- sites/ledsmagazine/config/site.js
+++ sites/ledsmagazine/config/site.js
@@ -77,7 +77,7 @@ module.exports = {
   subscriptions: {
     newsletters: 'https://formdesigner.ecn5.com/GetForm?tokenuid=25ab9782-5598-4ac2-af33-91a1d7ece79f&promoCode=WENL2019&utm_source=enl_sub&utm_medium=website&utm_campaign=WENL2019&utm_content=2019-02-25',
     publications: {
-      '/subscribe/print/leds': 'https://eforms.kmpsgroup.com/PaidForms/paidform.aspx?formid=94&promoCode-NW2019PD=&utm_source=mag_sub&utm_medium=website&utm_campaign=NW2019PD&utm_content=2019-02-25',
+      '/subscribe/print/leds': 'https://formdesigner.ecn5.com/GetForm?tokenuid=b238fdb4-ccc3-4438-9a6b-1ba1c898a9e1&promoCode=NW2019&utm_source=mag_sub&utm_medium=website&utm_campaign=NW2019&utm_content=2019-02-25',
     },
   },
   magazines: {
