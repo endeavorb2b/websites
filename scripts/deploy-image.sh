c239114a5bf0b4d5f4684ac77b0ea82fd55786cf
Jacob Bare <jacob@limit0.io>
2019-06-10 12:37:22 -0500

    Make docker builds loud!

--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -2,7 +2,7 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -q -t "$1:$2" --build-arg SITE=$1 .
+docker build -t "$1:$2" --build-arg SITE=$1 .
 
 docker tag "$1:$2" "endeavorb2b/website-$1:$2"
 docker push "endeavorb2b/website-$1:$2"
