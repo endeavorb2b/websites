00055e07785ae2c250870e07f3077e58b9c24c2e
Josh Worden <solocommand@gmail.com>
2019-06-10 11:48:40 -0500

    Update build to remove moving logic

--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -2,14 +2,7 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-mv sites ../
-mkdir sites
-mv ../sites/$1 sites/
-
 docker build -q -t "$1:$2" --build-arg SITE=$1 .
-
-mv ../sites/* sites/
-rm -rf ../sites
 
 docker tag "$1:$2" "endeavorb2b/website-$1:$2"
 docker push "endeavorb2b/website-$1:$2"
