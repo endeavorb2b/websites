f837b83b9e7cc686bddd10fe49280c7bb41c22f0
Jacob Bare <jacob@limit0.io>
2019-06-10 09:01:08 -0500

    Add complete and needsInput steps; disable on load

--- packages/identity-x/browser/components/sign-in/form.vue
+++ packages/identity-x/browser/components/sign-in/form.vue
@@ -1,14 +1,31 @@
 <template>
   <div v-if="hasActiveUser">
     <p>You are currently logged in as {{ activeUser.email }}.</p>
-    <a :href="logoutEndpoint" class="btn btn-primary" role="button">Logout</a>
+    <a :href="logoutEndpoint" class="btn btn-primary" role="button">{{ logoutButtonLabel }}</a>
+  </div>
+  <div v-else-if="complete">
+    <h4>Almost Done!</h4>
+    <p>We just sent an email to <em>{{ user.email }}</em> with your one-time login link. To finish logging in, open the email message and click the link within.</p>
+    <p>Note: please make sure you check your spam and or clutter/junk folders.</p>
+  </div>
+  <div v-else-if="needsInput">
+    <p>To complete this sign-on process, please fill out these remaining fields.</p>
+    <form @submit.prevent="handle">
+      <fieldset :disabled="loading">
+        <given-name v-model="user.givenName" />
+        <family-name v-model="user.familyName" />
+        <button type="submit" class="btn btn-primary">{{ buttonLabel }}</button>
+      </fieldset>
+      <p v-show="error">An error occurred: {{ error }}</p>
+    </form>
   </div>
   <div v-else>
     <form @submit.prevent="handle">
-      <email v-model="user.email" />
-      <given-name v-model="user.givenName" />
-      <family-name v-model="user.familyName" />
-      <button type="submit" class="btn btn-primary">{{ buttonLabel }}</button>
+      <fieldset :disabled="loading">
+        <email v-model="user.email" />
+        <button type="submit" class="btn btn-primary">{{ buttonLabel }}</button>
+      </fieldset>
+      <p v-show="error">An error occurred: {{ error }}</p>
     </form>
   </div>
 </template>
@@ -26,6 +43,10 @@ export default {
   },
   props: {
     activeUser: Object,
+    authEndpoint: {
+      type: String,
+      default: '/user/authenticate',
+    },
     context: {
       type: String,
       required: true,
@@ -34,6 +55,14 @@ export default {
     loginButtonLabel: {
       type: String,
       default: 'Login',
+    },
+    loginEndpoint: {
+      type: String,
+      default: '/user/login',
+    },
+    logoutButtonLabel: {
+      type: String,
+      default: 'Logout',
     },
     logoutEndpoint: {
       type: String,
@@ -49,12 +78,16 @@ export default {
     },
   },
   data: () => ({
+    complete: false,
+    error: null,
     loading: false,
+    needsInput: false,
     user: {},
   }),
   computed: {
     buttonLabel() {
       if (this.loading) return 'Loading...';
+      if (this.needsInput) return 'Finish';
       if (this.isLoginContext) return this.loginButtonLabel;
       if (this.isRegisterContext) return this.registerButtonLabel;
       return 'Continue';
@@ -68,9 +101,16 @@ export default {
     isRegisterContext() {
       return this.context === 'registier';
     },
+    redirectTo() {
+      const { pathname } = window.location;
+      const endpoints = [this.loginEndpoint, this.logoutEndpoint];
+      return pathname === endpoints.includes(pathname) ? undefined : pathname;
+    },
   },
   methods: {
     async handle() {
+      this.error = null;
+      this.loading = true;
       Object.keys(this.user).map(key => console.log(key, this.user[key]));
     },
   },
