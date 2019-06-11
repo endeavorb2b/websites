20e1cf831df2c0e291ef7b3710d77939d4b99949
Alysha Buselt <AlyshaB@Pennwell.com>
2019-06-11 10:03:00 -0500

    updated link to Front Office Forms
    https://southcomm.atlassian.net/projects/CS/queues/custom/12/CS-2288

--- sites/dentistryiq/config/site.js
+++ sites/dentistryiq/config/site.js
@@ -47,7 +47,7 @@ module.exports = {
   ],
   menuItems: {
     resources: [
-      { href: '/front-office/office-forms', label: 'Forms' },
+      { href: '/front-office/office-forms', label: 'Front Office Forms' },
       { href: '/magazine', label: 'Magazine' },
       { href: '/videos', label: 'Videos' },
       { href: '/white-papers', label: 'White Papers' },
