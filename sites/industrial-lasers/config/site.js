51820e17770e0a174c91f42b19e3af79dafc0e1d
Alysha Buselt <AlyshaB@Pennwell.com>
2019-06-12 09:33:03 -0500

    CS2301-ILS: also had LP Market Seminar in 2 navs

--- sites/industrial-lasers/config/site.js
+++ sites/industrial-lasers/config/site.js
@@ -39,7 +39,7 @@ module.exports = {
     { href: '/white-papers', label: 'White Papers' },
     { href: '/webcasts', label: 'Webcasts' },
     { href: 'https://buyersguide.industrial-lasers.com/index.html', label: 'Buyer\'s Guide', target: '_blank' },
-    { href: 'https://www.marketplaceseminar.com/index.html', label: 'LP Market Seminar', target: '_blank' },
+    { href: 'https://www.marketplaceseminar.com/index.html', label: 'L&P Market Seminar', target: '_blank' },
   ],
   footerNavItems: [
     { href: '/page/about-us', label: 'About Us' },
