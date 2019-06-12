22c015a87021c8ffb09e7695fb45acbd222644ca
Alysha Buselt <AlyshaB@Pennwell.com>
2019-06-12 16:07:47 -0500

    reverted ils changes

--- sites/industrial-lasers/config/site.js
+++ sites/industrial-lasers/config/site.js
@@ -39,7 +39,7 @@ module.exports = {
     { href: '/white-papers', label: 'White Papers' },
     { href: '/webcasts', label: 'Webcasts' },
     { href: 'https://buyersguide.industrial-lasers.com/index.html', label: 'Buyer\'s Guide', target: '_blank' },
-    { href: 'https://www.marketplaceseminar.com/index.html', label: 'L&P Market Seminar', target: '_blank' },
+    { href: 'https://www.marketplaceseminar.com/index.html', label: 'LP Market Seminar', target: '_blank' },
   ],
   footerNavItems: [
     { href: '/page/about-us', label: 'About Us' },
@@ -55,8 +55,7 @@ module.exports = {
       { href: '/white-papers', label: 'White Papers' },
       { href: '/webcasts', label: 'Webcasts' },
       { href: 'https://buyersguide.industrial-lasers.com/index.html', label: 'Buyer\'s Guide', target: '_blank' },
-      { href: 'https://www.marketplaceseminar.com/index.html', label: 'L&P Market Seminar', target: '_blank' },
-      { href: 'https://www.strategies-u.com/reports', label: 'Market Reports', target: '_blank' },
+      { href: 'https://www.marketplaceseminar.com/index.html', label: 'LP Market Seminar', target: '_blank' },
     ],
     userTools: [
       { href: '/subscribe', label: 'Subscribe' },
