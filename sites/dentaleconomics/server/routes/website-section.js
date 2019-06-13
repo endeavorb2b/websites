21e7732febc974e34780e6b7068b804ee363449e
Alysha Buselt <AlyshaB@Pennwell.com>
2019-06-13 17:10:38 -0500

    Refactored DE

--- sites/dentaleconomics/server/routes/website-section.js
+++ sites/dentaleconomics/server/routes/website-section.js
@@ -1,10 +1,18 @@
+const submissionHandler = require('@endeavorb2b/base-website-common/utils/contact-us');
 const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
+const contactUsTemplate = require('../templates/website-section/contact-us');
 
 module.exports = (app) => {
+  app.get('/:alias(contact-us)', withWebsiteSection({
+    template: contactUsTemplate,
+    queryFragment,
+  }));
+  submissionHandler(app);
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
   }));
+  submissionHandler(app);
 };
