deacac0609c687952884219570e2d1e62cf7acb3
Brandon Krigbaum <brandonbk@gmail.com>
2019-06-13 09:53:46 -0500

    OGJ home page blocks, native-x configuration

---
+++ sites/ogj/config/native-x.js
@@ -0,0 +1,36 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://ebm.native-x.io',
+  placements: {
+    default: {
+      list1: { id: '5d025a0ea7fa4000011a2ff0' },
+      list2: { id: '5d025a05e6eafd0001d4a3da' },
+      card: { id: '5d0259dde6eafd0001d499b5' },
+    },
+    'general-interest': {
+      list1: { id: '5d025813a7fa40000119a29b' },
+      list2: { id: '5d02581ea7fa40000119a57b' },
+      card: { id: '5d025825e6eafd0001d41ffd' },
+    },
+    'exploration-development': {
+      list1: { id: '5d0260774b58010001a2e894' },
+      list2: { id: '5d0260724b58010001a2e74a' },
+      card: { id: '5d02606b4b58010001a2e595' },
+    },
+    'drilling-production': {
+      list1: { id: '5d026142a7fa4000011c6afd' },
+      list2: { id: '5d02613e4b58010001a31c41' },
+      card: { id: '5d02614b4b58010001a32090' },
+    },
+    'refining-processing': {
+      list1: { id: '5d0261aba7fa4000011c8894' },
+      list2: { id: '5d0261b0a7fa4000011c8a85' },
+      card: { id: '5d0261b44b58010001a33eb4' },
+    },
+    'pipelines-transportation': {
+      list1: { id: '5d0262244b58010001a36615' },
+      list2: { id: '5d02621f4b58010001a364e5' },
+      card: { id: '5d026228a7fa4000011cb5a8' },
+    },
+  },
+};
