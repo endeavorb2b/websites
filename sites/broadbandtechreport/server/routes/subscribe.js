e42beedfb1ba2fb687b80ce58ed9ab370413ea53
Alysha Buselt <AlyshaB@Pennwell.com>
2019-06-21 12:49:36 -0500

    BC-BTR-IAS: removed mag template information
    Got in a hurry and missed the most important parts!

--- sites/broadbandtechreport/server/routes/subscribe.js
+++ sites/broadbandtechreport/server/routes/subscribe.js
@@ -1,8 +1,5 @@
-const { withMagazineSubscribeUrl } = require('@base-cms/marko-web/middleware');
-const queryFragment = require('@endeavorb2b/base-website-themes/pennwell/api/fragments/magazine-publication');
 const subscribe = require('../templates/subscribe/index');
 const subscribeEmail = require('../templates/subscribe/email');
-const subscribeMagazine = require('../templates/subscribe/magazine');
 
 module.exports = (app) => {
   app.get('/subscribe', (_, res) => {
@@ -12,9 +9,4 @@ module.exports = (app) => {
   app.get('/subscribe/email', (_, res) => {
     res.marko(subscribeEmail);
   });
-
-  app.get(':subscribeUrl(/subscribe/print/[a-zA-Z]+)', withMagazineSubscribeUrl({
-    template: subscribeMagazine,
-    queryFragment,
-  }));
 };
