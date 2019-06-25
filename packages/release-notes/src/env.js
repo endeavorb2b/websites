7696dfe1c90f9198a5c7a4243dfb4394d19b070e
Jacob Bare <jacob@limit0.io>
2019-06-25 14:58:05 -0500

    Remove GITHUB_REPO env var

--- packages/release-notes/src/env.js
+++ packages/release-notes/src/env.js
@@ -7,5 +7,4 @@ const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
   GITHUB_PERSONAL_ACCESS_TOKEN: nonemptystr({ desc: 'A GitHub personal access token.' }),
-  GITHUB_REPO: nonemptystr({ desc: 'The GitHub repo to access', default: 'endeavorb2b/websites' }),
 });
