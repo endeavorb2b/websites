acf5cbab48e975c9a5cd3f4bd495fe9c8f3ffaf1
Jacob Bare <jacob@limit0.io>
2019-07-09 11:00:39 -0500

    Fix lint error

--- sites/bizbash/config/ads.js
+++ sites/bizbash/config/ads.js
@@ -1,23 +1,23 @@
-const BS = {
-  size: [[970, 250], [970, 90], [728, 90], [320, 50], [300, 50], [300, 100]],
-  sizeMapping: [
-    { viewport: [980, 0], size: [[970, 250], [970, 90], [728, 90]] },
-    { viewport: [750, 0], size: [728, 90] },
-    { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
-  ],
-};
-const MR = {
-  size: [300, 250],
-};
-const HP = {
-  size: [300, 600],
-};
-const LB = {};
+// const BS = {
+//   size: [[970, 250], [970, 90], [728, 90], [320, 50], [300, 50], [300, 100]],
+//   sizeMapping: [
+//     { viewport: [980, 0], size: [[970, 250], [970, 90], [728, 90]] },
+//     { viewport: [750, 0], size: [728, 90] },
+//     { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
+//   ],
+// };
+// const MR = {
+//   size: [300, 250],
+// };
+// const HP = {
+//   size: [300, 600],
+// };
+// const LB = {};
 
-const RESKIN = {};
-const WA = {};
+// const RESKIN = {};
+// const WA = {};
 
-const path = unit => `/21802476813/bizbash.com/${unit}`;
+// const path = unit => `/21802476813/bizbash.com/${unit}`;
 
 module.exports = {
   default: {
