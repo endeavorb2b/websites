e83993d2e203afbd07832ff655e9a34d1c1dba63
Josh Worden <solocommand@gmail.com>
2019-07-11 08:52:39 -0500

    Remove duplicate bodyParser

--- packages/inquiry/routers/index.js
+++ packages/inquiry/routers/index.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
 const { json } = require('body-parser');
 const jsonErrorHandler = require('@endeavorb2b/base-website-routing-utils/json-error-handler');
-const bodyParser = require('body-parser');
 const submit = require('./submit');
 
 module.exports = ({ queryFragment, notification, confirmation }) => {
   const router = Router();
   router.use(json());
-  router.post('/:id(\\d{8})', bodyParser.json(), submit({ queryFragment, notification, confirmation }));
+  router.post('/:id(\\d{8})', submit({ queryFragment, notification, confirmation }));
   router.use(jsonErrorHandler());
   return router;
 };
