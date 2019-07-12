f4339029dbfbf25854e32cddfa8ec5c30961b098
Jacob Bare <jacob@limit0.io>
2019-07-12 09:07:01 -0500

    Allow three attributes per line

--- packages/identity-x/browser/.eslintrc.js
+++ packages/identity-x/browser/.eslintrc.js
@@ -6,4 +6,13 @@ module.exports = {
   env: {
     browser: true,
   },
+  rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
+  },
 };
