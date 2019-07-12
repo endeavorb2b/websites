2dc43761a6ecb2924893047b1e8904ab0e47abdd
Josh Worden <solocommand@gmail.com>
2019-07-12 09:22:03 -0500

    Initial carousel component

--- sites/bizbash/browser/index.js
+++ sites/bizbash/browser/index.js
@@ -1,3 +1,6 @@
 import Browser from '@endeavorb2b/base-website-common/browser';
+import RelatedImageCarousel from './related-image-carousel.vue';
+
+Browser.registerComponent('RelatedImageCarousel', RelatedImageCarousel);
 
 export default Browser;
