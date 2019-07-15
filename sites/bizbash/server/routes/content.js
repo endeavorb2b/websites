347e80a66aff6874bf150d6cd75e28fef198dfe8
Josh Worden <solocommand@gmail.com>
2019-07-15 13:10:48 -0500

    Add support for venues and spaces table
    # Conflicts:
    #	sites/bizbash/components/marko.json
    #	sites/bizbash/server/styles/index.scss
    
    # Conflicts:
    #	sites/bizbash/server/routes/content.js

--- sites/bizbash/server/routes/content.js
+++ sites/bizbash/server/routes/content.js
@@ -3,6 +3,7 @@ const queryFragment = require('../api/fragments/content-page');
 const content = require('../templates/content');
 const supplier = require('../templates/content/supplier');
 const topList = require('../templates/content/top-list');
+const venue = require('../templates/content/venue');
 
 module.exports = (app) => {
   app.get('/*?supplier/:id(\\d{8})*', withContent({
@@ -11,6 +12,10 @@ module.exports = (app) => {
   }));
   app.get('/*?top-list/:id(\\d{8})*', withContent({
     template: topList,
+    queryFragment,
+  }));
+  app.get('/*?venue/:id(\\d{8})*', withContent({
+    template: venue,
     queryFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
