7fcff4bdf361e59c42af138e6092e104dcf7a9f0
Brandon Krigbaum <brandonbk@gmail.com>
2019-07-17 16:00:55 -0500

    Fixed linting error

--- packages/common/components/navbars/get-nav-items.js
+++ packages/common/components/navbars/get-nav-items.js
@@ -2,19 +2,17 @@ module.exports = ({
   regEnabled,
   hasUser,
   items,
-}) => {
-  return items.filter((item) => {
-    const { when } = item;
-    if (!when) return true;
-    if (!regEnabled) return false;
-    if (hasUser) {
-      if (when === 'logged-in') return true;
-      if (when === 'logged-out') return false;
-    }
-    if (!hasUser) {
-      if (when === 'logged-in') return false;
-      if (when === 'logged-out') return true;
-    }
-    return false;
-  });
-};
+}) => items.filter((item) => {
+  const { when } = item;
+  if (!when) return true;
+  if (!regEnabled) return false;
+  if (hasUser) {
+    if (when === 'logged-in') return true;
+    if (when === 'logged-out') return false;
+  }
+  if (!hasUser) {
+    if (when === 'logged-in') return false;
+    if (when === 'logged-out') return true;
+  }
+  return false;
+});
