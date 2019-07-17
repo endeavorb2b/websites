a6aea1bbd819874dad5a46acc707b618e63f4808
Josh Worden <solocommand@gmail.com>
2019-07-17 10:06:36 -0500

    Remove defaults

--- sites/bizbash/components/image-format.js
+++ sites/bizbash/components/image-format.js
@@ -1,15 +1,6 @@
 const { buildImgixUrl } = require('@base-cms/image');
 
-const defaults = {
-  fit: 'crop',
-  crop: 'focalpoint',
-  fpX: 0.5,
-  fpY: 0.5,
-  w: 680,
-  h: 380,
-};
-
 module.exports = (image, options = {}) => {
-  const src = buildImgixUrl(image.src, { ...defaults, ...options });
+  const src = buildImgixUrl(image.src, options);
   return { ...image, src };
 };
