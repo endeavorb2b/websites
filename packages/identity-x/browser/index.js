33660bf7d67d312e30ed61ecc23824177233b2b6
Josh Worden <solocommand@gmail.com>
2019-07-18 09:31:33 -0500

    Change defaults and allow sentry to be disabled or
    have the DSN overwritten.

--- packages/identity-x/browser/index.js
+++ packages/identity-x/browser/index.js
@@ -5,11 +5,12 @@ import components from './components';
 import createClient from './create-client';
 
 const isProd = window.location.hostname !== '0.0.0.0';
+const dsn = 'https://7d01088298a44760a0e91923b1f17933@sentry.as3.io/17';
 
-export default (Browser, { mountPoint, sentryDsn } = {}) => {
-  if (sentryDsn) {
+export default (Browser, { mountPoint, sentryDisabled, sentryDsn } = {}) => {
+  if (!sentryDisabled) {
     Sentry.init({
-      dsn: sentryDsn,
+      dsn: sentryDsn || dsn,
       integrations: [new Integrations.Vue({ Vue, attachProps: true })],
       environment: isProd ? 'production' : 'development',
       debug: !isProd,
--- sites/ogj/browser/index.js
+++ sites/ogj/browser/index.js
@@ -1,8 +1,6 @@
 import Browser from '@endeavorb2b/base-website-common/browser';
 import identityX from '@endeavorb2b/base-website-identity-x/browser';
 
-const sentryDsn = 'https://7d01088298a44760a0e91923b1f17933@sentry.as3.io/17';
-
-identityX(Browser, { sentryDsn });
+identityX(Browser);
 
 export default Browser;
