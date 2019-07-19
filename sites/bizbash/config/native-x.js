aa633fb8e8eb077b9b6149a5eb1ed856babfa9b2
Brandon Krigbaum <brandonbk@gmail.com>
2019-07-18 21:07:57 -0500

    Added native-x.js for bizbash

---
+++ sites/bizbash/config/native-x.js
@@ -0,0 +1,36 @@
+module.exports = {
+  enabled: true,
+  uri: 'https://bbm.native-x.io',
+  placements: {
+    catering: {
+      list2: { id: '5d30cc522ab3e700012d8003' },
+      list1: { id: '5d30cc4c2ab3e700012d7fd4' },
+      card: { id: '5d30cc30990f520001091bcb' },
+    },
+    default: {
+      list2: { id: '5d30cfb22ab3e700012d8fce' },
+      list1: { id: '5d30cfd12ab3e700012d90c2' },
+      card: { id: '5d30cfe5990f520001092d7f' },
+    },
+    'style-decor': {
+      list2: { id: '5d30cc762ab3e700012d80ea' },
+      list1: { id: '5d30cc7b990f520001091d83' },
+      card: { id: '5d30cc712ab3e700012d80d0' },
+    },
+    'production-strategy': {
+      list2: { id: '5d30cc892ab3e700012d8169' },
+      list1: { id: '5d30cc8e990f520001091e1e' },
+      card: { id: '5d30cc85990f520001091dcc' },
+    },
+    'local-venues-destinations': {
+      list2: { id: '5d30cc522ab3e700012d8003' },
+      list1: { id: '5d30cc4c2ab3e700012d7fd4' },
+      card: { id: '5d30cc30990f520001091bcb' },
+    },
+    'bizbash-lists': {
+      list2: { id: '5d30cd402ab3e700012d84ba' },
+      list1: { id: '5d30cd4e2ab3e700012d850c' },
+      card: { id: '5d30cd44990f52000109210e' },
+    },
+  },
+};
