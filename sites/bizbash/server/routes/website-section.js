bc0d3d5092965751e555680d3313dd86436cfe44
Brandon Krigbaum <brandonbk@gmail.com>
2019-07-19 08:10:55 -0500

    Added route and custom page for GatherGeeks

--- sites/bizbash/server/routes/website-section.js
+++ sites/bizbash/server/routes/website-section.js
@@ -3,6 +3,7 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const submissionHandler = require('@endeavorb2b/base-website-common/utils/contact-us');
 const section = require('../templates/website-section');
 const contactUsTemplate = require('../templates/website-section/contact-us');
+const gatherGeeksTemplate = require('../templates/website-section/gathergeeks');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
@@ -10,6 +11,12 @@ module.exports = (app) => {
     queryFragment,
   }));
   submissionHandler(app);
+
+  app.get('/:alias(gathergeeks)', withWebsiteSection({
+    template: gatherGeeksTemplate,
+    queryFragment,
+  }));
+
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
