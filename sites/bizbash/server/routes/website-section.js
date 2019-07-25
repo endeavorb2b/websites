42daea704c7115776899c3f0122f8faf286381c4
Jacob Bare <jacob@limit0.io>
2019-07-25 07:27:41 -0500

    Remove contact us section route

--- sites/bizbash/server/routes/website-section.js
+++ sites/bizbash/server/routes/website-section.js
@@ -1,20 +1,12 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/default/api/fragments/website-section-page');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
-const submissionHandler = require('@endeavorb2b/base-website-common/utils/contact-us');
 const section = require('../templates/website-section');
-const contactUsTemplate = require('../templates/website-section/contact-us');
 const gatherGeeksTemplate = require('../templates/website-section/gathergeeks');
 const productionStrategyTemplate = require('../templates/website-section/production-strategy');
 const styleDecorTemplate = require('../templates/website-section/style-decor');
 const cateringTemplate = require('../templates/website-section/catering');
 
 module.exports = (app) => {
-  app.get('/:alias(contact-us)', withWebsiteSection({
-    template: contactUsTemplate,
-    queryFragment,
-  }));
-  submissionHandler(app);
-
   app.get('/:alias(gathergeeks)', withWebsiteSection({
     template: gatherGeeksTemplate,
     queryFragment,
