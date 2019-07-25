ec6b8b7a2ec2d95493aaa0de0030d59b8f27e029
Jacob Bare <jacob@limit0.io>
2019-07-25 10:52:13 -0500

    Change facet object format

--- sites/bizbash/components/directory-search/markets.js
+++ sites/bizbash/components/directory-search/markets.js
@@ -1,87 +1,84 @@
-module.exports = {
-  name: 'Market',
-  values: [
-    {
-      id: 2000027,
-      name: 'Atlanta',
-    },
-    {
-      id: 2000028,
-      name: 'Atlantic City',
-    },
-    {
-      id: 2000029,
-      name: 'Austin',
-    },
-    {
-      id: 2000023,
-      name: 'Boston',
-    },
-    {
-      id: 2000021,
-      name: 'Chicago',
-    },
-    {
-      id: 2000030,
-      name: 'Dallas/Fort Worth',
-    },
-    {
-      id: 2000031,
-      name: 'Houston',
-    },
+module.exports = [
+  {
+    id: 2000027,
+    name: 'Atlanta',
+  },
+  {
+    id: 2000028,
+    name: 'Atlantic City',
+  },
+  {
+    id: 2000029,
+    name: 'Austin',
+  },
+  {
+    id: 2000023,
+    name: 'Boston',
+  },
+  {
+    id: 2000021,
+    name: 'Chicago',
+  },
+  {
+    id: 2000030,
+    name: 'Dallas/Fort Worth',
+  },
+  {
+    id: 2000031,
+    name: 'Houston',
+  },
 
-    {
-      id: 2000024,
-      name: 'Las Vegas',
-    },
-    {
-      id: 2000017,
-      name: 'Los Angeles',
-    },
-    {
-      id: 2000025,
-      name: 'Miami/South Florida',
-    },
-    {
-      id: 2000038,
-      name: 'Montreal',
-    },
-    {
-      id: 2000016,
-      name: 'New York',
-    },
-    {
-      id: 2000026,
-      name: 'Orlando/Central Florida',
-    },
-    {
-      id: 2000034,
-      name: 'Philadelphia',
-    },
-    {
-      id: 2000035,
-      name: 'Phoenix/Scottsdale',
-    },
-    {
-      id: 2000036,
-      name: 'San Diego',
-    },
-    {
-      id: 2000037,
-      name: 'San Francisco',
-    },
-    {
-      id: 2000020,
-      name: 'Toronto',
-    },
+  {
+    id: 2000024,
+    name: 'Las Vegas',
+  },
+  {
+    id: 2000017,
+    name: 'Los Angeles',
+  },
+  {
+    id: 2000025,
+    name: 'Miami/South Florida',
+  },
+  {
+    id: 2000038,
+    name: 'Montreal',
+  },
+  {
+    id: 2000016,
+    name: 'New York',
+  },
+  {
+    id: 2000026,
+    name: 'Orlando/Central Florida',
+  },
+  {
+    id: 2000034,
+    name: 'Philadelphia',
+  },
+  {
+    id: 2000035,
+    name: 'Phoenix/Scottsdale',
+  },
+  {
+    id: 2000036,
+    name: 'San Diego',
+  },
+  {
+    id: 2000037,
+    name: 'San Francisco',
+  },
+  {
+    id: 2000020,
+    name: 'Toronto',
+  },
 
-    {
-      id: 2000018,
-      name: 'Washington, D.C.',
-    },
-    {
-      id: 3125552,
-      name: 'Vancouver',
-    },
-  ],
-};
+  {
+    id: 2000018,
+    name: 'Washington, D.C.',
+  },
+  {
+    id: 3125552,
+    name: 'Vancouver',
+  },
+];
