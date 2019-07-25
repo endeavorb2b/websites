5fde4a895485e9d5a1782f663de0f789ec08a42a
Jacob Bare <jacob@limit0.io>
2019-07-25 13:29:33 -0500

    Handle active facet classinfg

--- sites/bizbash/components/directory-search/find-active-facet.js
+++ sites/bizbash/components/directory-search/find-active-facet.js
@@ -1,12 +1,19 @@
-const findActiveFacet = (facets, facetId) => {
+const findActiveFacet = (facets, facetId, tree = []) => {
   if (!facetId) return undefined;
+
   return facets.reduce((active, facet) => {
     const { id, values } = facet;
     if (active) return active;
-    if (`${facetId}` === `${id}`) return facet;
-    if (Array.isArray(values) && values.length) return findActiveFacet(values, facetId);
+    if (`${facetId}` === `${id}`) {
+      tree.push({ ...facet });
+      return { facet: { ...facet }, tree };
+    }
+    if (Array.isArray(values) && values.length) {
+      tree.push({ ...facet });
+      return findActiveFacet(values, facetId, tree);
+    }
     return active;
-  }, undefined);
+  }, undefined) || { facet: null, tree: [] };
 };
 
 module.exports = findActiveFacet;
