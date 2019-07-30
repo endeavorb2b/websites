ee0fdba657ef6ce5bb9af3a3309d238ac63049f9
Jacob Bare <jacob@limit0.io>
2019-07-30 07:49:06 -0500

    Create generic menu toggle vue component

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,4 +1,5 @@
 import Browser from '@base-cms/marko-web/browser';
+import MenuToggleButton from './menu-toggle-button.vue';
 import NavbarToggleButton from './navbar-toggle-button.vue';
 import PlusMinusToggleButton from './plus-minus-toggle-button.vue';
 import SearchIntegrationBing from './search-integration-bing.vue';
@@ -9,6 +10,7 @@ import ContactUsForm from './contact-us-form.vue';
 import ReskinListener from './reskin-listener.vue';
 import StickyLeaderboard from './sticky-leaderboard.vue';
 
+Browser.registerComponent('MenuToggleButton', MenuToggleButton);
 Browser.registerComponent('NavbarToggleButton', NavbarToggleButton);
 Browser.registerComponent('PlusMinusToggleButton', PlusMinusToggleButton);
 Browser.registerComponent('SearchIntegrationBing', SearchIntegrationBing);
