2b77f863768fd1a45cf3d65b4b8ebe13fe5bc7bd
Brian Miller <bmiller@southcomm.com>
2019-08-16 09:11:11 -0500

    rename fragment

--- sites/mundopmmi/server/api/fragments/featured-companies.js
+++ sites/mundopmmi/server/api/fragments/featured-companies.js
@@ -2,7 +2,7 @@ const gql = require('graphql-tag');
 
 module.exports = gql`
 
-fragment mundopmmiContentListFragment on Content {
+fragment MundoPMMIFeaturedCompaniesFragment on Content {
   id
   type
   typeTitled: type(input: { format: titleize })
