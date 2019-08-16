e3074f077e8d7f24f203f9126ca5f85319737a48
Brian Miller <bmiller@southcomm.com>
2019-08-16 09:09:08 -0500

    cleanup

--- sites/mundopmmi/server/api/fragments/content-list.js
+++ sites/mundopmmi/server/api/fragments/content-list.js
@@ -7,37 +7,7 @@ fragment mundopmmiContentListFragment on Content {
   type
   typeTitled: type(input: { format: titleize })
   shortName
-  teaser(input: { maxLength: 500, useFallback: false, truncatedSuffix: "" })
   canonicalPath
-  published
-  primarySection {
-    id
-    name
-    fullName
-    canonicalPath
-  }
-  primaryImage {
-    id
-    src
-    alt
-    isLogo
-  }
-  ... on ContentPromotion {
-    linkText
-    linkUrl
-  }
-  ... on ContentContact {
-    title
-    phone
-    publicEmail
-  }
-  ... on ContentWebinar {
-    starts
-  }
-  ... on ContentEvent {
-    starts
-    ends
-  }
 }
 
 `;
