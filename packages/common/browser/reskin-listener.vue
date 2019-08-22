4148f5bcfc268dcb4eb2b17b303497f609ee5a94
Brian Miller <bmiller@southcomm.com>
2019-08-22 13:27:10 -0500

    update reskin logic to support target prop

--- packages/common/browser/reskin-listener.vue
+++ packages/common/browser/reskin-listener.vue
@@ -13,49 +13,57 @@ const parseJson = (str) => {
   }
 };
 
-const defaults = {
-  backgroundColor: 'transparent',
-};
-
-const display = (payload) => {
-  const options = { ...defaults, ...payload };
-  const { adClickUrl: href, backgroundColor, backgroundImagePath } = options;
-  const { adImagePath: src, adTitle: alt } = options;
-  const backgroundImage = `url("${backgroundImagePath}")`;
-
-  const title = alt;
-  const target = '_blank';
-  const rel = 'noopener noreferrer';
-
-  const adContainer = $('<div>').addClass('reveal-ad');
-  if (options.boxShadow) adContainer.addClass(`reveal-ad--${options.boxShadow}-shadow`);
-  adContainer.html($('<a>', {
-    href,
-    title,
-    target,
-    rel,
-  }).append($('<img>', { src, alt })));
-
-  const revealBackground = $('<a>', { href, target, rel }).addClass('reveal-ad-background').css({ backgroundImage });
-  $('body').css({ backgroundColor }).prepend(revealBackground);
-  $('.container-fluid-max > .ad-container').before(adContainer);
-  $('.container-fluid-max').append(adContainer.clone());
-};
-
-const listener = (e) => {
-  const payload = parseJson(e.data);
-  if (['adImagePath', 'adTitle', 'backgroundImagePath', 'adClickUrl'].every(k => payload[k])) {
-    display(payload);
-    window.removeEventListener('message', listener);
-  }
-};
-
 export default {
+  props: {
+    target: {
+      type: String,
+      default: '.container-fluid-max .page-section',
+    },
+    defaults: {
+      type: Object,
+      default: () => ({ backgroundColor: 'transparent' }),
+    },
+  },
   created() {
-    window.addEventListener('message', listener);
+    window.addEventListener('message', this.listener);
   },
   beforeDestroy() {
-    window.removeEventListener('message', listener);
+    window.removeEventListener('message', this.listener);
+  },
+  methods: {
+    display(payload) {
+      const options = { ...this.defaults, ...payload };
+      const adTarget = this.target;
+      const { adClickUrl: href, backgroundColor, backgroundImagePath } = options;
+      const { adImagePath: src, adTitle: alt } = options;
+      const backgroundImage = `url("${backgroundImagePath}")`;
+
+      const title = alt;
+      const target = '_blank';
+      const rel = 'noopener noreferrer';
+
+      const adContainer = $('<div>').addClass('reveal-ad');
+      if (options.boxShadow) adContainer.addClass(`reveal-ad--${options.boxShadow}-shadow`);
+      adContainer.html($('<a>', {
+        href,
+        title,
+        target,
+        rel,
+      }).append($('<img>', { src, alt })));
+
+      const revealBackground = $('<a>', { href, target, rel }).addClass('reveal-ad-background').css({ backgroundImage });
+      $('body').css({ backgroundColor }).prepend(revealBackground);
+      // Figure this out
+      $(adTarget).before(adContainer);
+      $(this.target).append(adContainer.clone());
+    },
+    listener(event) {
+      const payload = parseJson(event.data);
+      if (['adImagePath', 'adTitle', 'backgroundImagePath', 'adClickUrl'].every(k => payload[k])) {
+        this.display(payload);
+        window.removeEventListener('message', this.listener);
+      }
+    },
   },
 };
 </script>
