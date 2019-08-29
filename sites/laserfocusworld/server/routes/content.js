436a6a622a79c5708d36caa3795bb69c0e03f877
Brandon Krigbaum <brandonbk@gmail.com>
2019-08-29 10:37:45 -0500

    Added custom whitepaper route and layout

--- sites/laserfocusworld/server/routes/content.js
+++ sites/laserfocusworld/server/routes/content.js
@@ -1,8 +1,13 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/default/api/fragments/content-page');
 const { withContent } = require('@base-cms/marko-web/middleware');
+const whitepaper = require('../templates/content/whitepaper');
 const content = require('../templates/content');
 
 module.exports = (app) => {
+  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
+    template: whitepaper,
+    queryFragment,
+  }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
