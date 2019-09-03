741754be05dfe675b848266eb204fb63d0a15360
Brandon Krigbaum <brandonbk@gmail.com>
2019-09-03 14:30:55 -0500

    Moved whitepaper template to core; updated routes

--- sites/forconstructionpros/server/routes/content.js
+++ sites/forconstructionpros/server/routes/content.js
@@ -1,8 +1,13 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/default/api/fragments/content-page');
 const { withContent } = require('@base-cms/marko-web/middleware');
+const whitepaper = require('@endeavorb2b/base-website-themes/default/templates/whitepaper');
 const content = require('../templates/content');
 
 module.exports = (app) => {
+  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
+    template: whitepaper,
+    queryFragment,
+  }));
   app.get('/:prefix(*):id(\\d{8}):suffix(*)', withContent({
     template: content,
     queryFragment,
--- sites/laserfocusworld/server/routes/content.js
+++ sites/laserfocusworld/server/routes/content.js
@@ -1,6 +1,6 @@
 const queryFragment = require('@endeavorb2b/base-website-themes/default/api/fragments/content-page');
 const { withContent } = require('@base-cms/marko-web/middleware');
-const whitepaper = require('../templates/content/whitepaper');
+const whitepaper = require('@endeavorb2b/base-website-themes/default/templates/whitepaper');
 const content = require('../templates/content');
 
 module.exports = (app) => {
