6b064ab918223a94c58fb4cd5d1f52e78d20eddc
Alysha Buselt <>
2019-09-13 11:08:52 -0400

    CS3012-VSD: add knowledge zone category
    https://southcomm.atlassian.net/browse/CS-3012
    
    Initial layout commit, before submitter feedback
    
    Per Deb:
    
    Category: Knowledge Zone
    Sub-Cat: Knowledge Zone > Machine Vision Terminology
    Sub-Cat: Knowledge Zone > Machine Vision Tutorials
    Sub-Cat: Knowledge Zone > Ask the Expert
    
    - On the channel page, they would like to have content listings for the three sub-cats, webcasts, white papers and videos. The webcasts, white papers and videos will pull from those content types.
    
    - The channel will serve Home Page ads.
    
    - Add “Knowledge Zone” under “Resources” in the hamburger nav

--- sites/vision-systems/server/routes/website-section.js
+++ sites/vision-systems/server/routes/website-section.js
@@ -3,10 +3,15 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const submissionHandler = require('@endeavorb2b/base-website-common/utils/contact-us');
 const section = require('../templates/website-section');
 const contactUsTemplate = require('../templates/website-section/contact-us');
+const knowledgeZoneTemplate = require('../templates/website-section/knowledge-zone');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUsTemplate,
+    queryFragment,
+  }));
+  app.get('/:alias(knowledge-zone)', withWebsiteSection({
+    template: knowledgeZoneTemplate,
     queryFragment,
   }));
   submissionHandler(app);
