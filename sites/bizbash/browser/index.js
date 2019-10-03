448f463d5b6e53501102c2236fe04cf0f355bed0
Brandon Krigbaum <brandonbk@gmail.com>
2019-10-02 19:25:55 -0500

    Added jQuery to all the things

--- sites/bizbash/browser/index.js
+++ sites/bizbash/browser/index.js
@@ -1,9 +1,13 @@
 import Browser from '@endeavorb2b/base-website-common/browser';
 import inquiry from '@endeavorb2b/base-website-inquiry/browser';
+import jQuery from '@base-cms/marko-web/browser/jquery';
 import InquiryForm from './inquiry-form.vue';
 import ImageSlider from './image-slider.vue';
 
 inquiry(Browser, { component: InquiryForm });
 Browser.registerComponent('ImageSlider', ImageSlider);
+
+window.jQuery = window.$;
+window.$ = jQuery;
 
 export default Browser;
