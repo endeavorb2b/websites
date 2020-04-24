c6e2a566f6871dfe7ef2b1df7bf866b8c54d393b
Josh Worden <solocommand@gmail.com>
2020-04-24 09:31:28 -0500

    Increase footer logo height

--- sites/bizbash/config/site.js
+++ sites/bizbash/config/site.js
@@ -13,9 +13,9 @@ module.exports = {
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/bizbash/bzb/image/static/bizbash-new-footer.png?h=60',
+      src: 'https://base.imgix.net/files/base/bizbash/bzb/image/static/bizbash-new-footer.png?h=80',
       srcset: [
-        'https://base.imgix.net/files/base/bizbash/bzb/image/static/bizbash-new-footer.png?h=120 2x',
+        'https://base.imgix.net/files/base/bizbash/bzb/image/static/bizbash-new-footer.png?h=160 2x',
       ],
     },
   },
